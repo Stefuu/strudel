cab790d509f6cac6f0a8266e949f746c9c27f807
Stefano Damiano <stefano@ae.studio>
2025-06-17 19:39:44 -0300

    wip

--- examples/codemirror-repl/vite.config.js
+++ examples/codemirror-repl/vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@strudel/codemirror': resolve(__dirname, '../../packages/codemirror'),
-      '/doc.json': resolve(__dirname, '../../doc.json')
     }
   },
   build: {
     commonjsOptions: {
       include: [/node_modules/],
     }
-  }
+  },
+  publicDir: 'public'
 }); 
